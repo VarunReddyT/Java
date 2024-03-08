999e34313aba9da7cec27ab3e98ef06759c28859
Varun Reddy <tvarun2014@gmail.com>
2024-03-08 16:23:50 +0530

    Hello World

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,5 +1,6 @@
 public class HelloWorld1{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World");  // println - prints the string and moves to the next line
+        System.out.print("Hello World");   // print - prints the string and stays on the same line
     }
 }
