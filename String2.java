9151064e68b96a783d15b31c0de4ea1349bd29cd
Varun Reddy <tvarun2014@gmail.com>
2024-03-09 17:38:19 +0530

    String

---
+++ String2.java
@@ -0,0 +1,8 @@
+public class String2 {
+    public static void main(String[] args){
+        String s1 = "Hello";
+        String s2 = "World";
+        String s3 = s1 + " " + s2;
+        System.out.println(s3);
+    }
+}
