b3e20cc0e08dead10b104ec6e7db60d2c2799e61
Varun Reddy <tvarun2014@gmail.com>
2024-03-09 23:24:45 +0530

    Array

---
+++ Arrays5.java
@@ -0,0 +1,8 @@
+public class Arrays5 {
+    public static void main(String[] args){
+        int[] myArray = {1,2,3,4,5}; // Declaration different from C++
+        for(int i=0; i<myArray.length; i++){
+            System.out.print(myArray[i]+" ");
+        }
+    }
+}
