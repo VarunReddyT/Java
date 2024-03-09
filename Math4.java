9c361094e216ca5ce192fbc78b99d7418c9e48ad
Varun Reddy <tvarun2014@gmail.com>
2024-03-09 22:23:11 +0530

    Math

---
+++ Math4.java
@@ -0,0 +1,8 @@
+public class Math4{
+    public static void main(String[] args){
+        System.out.println(Math.max(5,10));
+        System.out.println(Math.min(5,10));
+        System.out.println(Math.sqrt(63));
+        System.out.println((int)(Math.random()*100));
+    }
+}
