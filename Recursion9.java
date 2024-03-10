3f332c34cc70112845f9484f916e8002710ea753
Varun Reddy <tvarun2014@gmail.com>
2024-03-10 23:48:29 +0530

    Recursion

---
+++ Recursion9.java
@@ -0,0 +1,13 @@
+public class Recursion9{
+    public static void main(String[] args) {
+      int result = sum(10);
+      System.out.println(result);
+    }
+    public static int sum(int k) {
+      if (k > 0) {
+        return k + sum(k - 1);
+      } else {
+        return 0;
+      }
+    }
+  }
