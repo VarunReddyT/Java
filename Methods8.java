72fc199b4fd8b93c59ed5d9f82e167e28300c43c
Varun Reddy <tvarun2014@gmail.com>
2024-03-10 23:46:58 +0530

    FunctionsOrMethods

---
+++ Methods8.java
@@ -0,0 +1,8 @@
+public class Methods8{
+    public static void main(String[] args){
+        printS();  // Function call
+    }
+    public static void printS(){ // Function definition
+        System.out.println("Hello, World!");
+    }
+}
