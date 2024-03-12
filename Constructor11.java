c24671a54968f8a08fb5f2dcd997ba0444e4dc7e
Varun Reddy <tvarun2014@gmail.com>
2024-03-12 23:41:06 +0530

    Constructor

--- Constructor11.java
+++ Constructor11.java
@@ -5,11 +5,15 @@ class Pen{
     Pen(){
         System.out.println("Constructor called");
     }
+    Pen(String color){
+        this.color = color;
+        System.out.println("Color of the pen is: "+color);
+    }
 }
 
 public class Constructor11 {
     public static void main(String[] args) {
         Pen p1 = new Pen();
-        p1.color = "Blue";
+        Pen p2 = new Pen("Blue");
     }
 }
