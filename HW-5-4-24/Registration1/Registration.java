afe8c48ccce797b3a085cc51225231431e0f6031
Varun-MSI <tvarun2014@gmail.com>
2024-04-05 22:48:43 +0530

    Registration

--- HW-5-4-24/Registration1/Registration.java
+++ HW-5-4-24/Registration1/Registration.java
@@ -16,96 +16,76 @@ public class Registration{
     private int licenseNo;
     private long[] telephoneNo;
 
-    public static Scanner input = new Scanner(System.in);
     Registration(String customerName, String passportNo, long[] telephoneNo){
         this.customerName = customerName;
         this.passportNo = passportNo;
         this.telephoneNo = telephoneNo;
-        System.out.println("Congratulations " + customerName + " You have been successfully registered for our services with the following details:");
-        System.out.println("Passport number : " + passportNo);
-        System.out.println("Phone numbers : " + telephoneNo[0] + "\n" + telephoneNo[1]);
+        System.out.println("Congratulations " + getCustomerName() + "!!! you have been successfully registered for our services with the following details:");
+        System.out.println("Passport number : " + getPassportNo());
+        System.out.println("Phone numbers : " + getTelephoneNo()[0] + "\n" + getTelephoneNo()[1]);
+        System.out.println();
     }
     Registration(String customerName, int licenseNo, String panCardNo,long[] telephoneNo ){
         this.customerName = customerName;
         this.licenseNo = licenseNo;
         this.panCardNo = panCardNo;
         this.telephoneNo = telephoneNo;
-        System.out.println("Congratulations " + customerName + " You have been successfully registered for our services with the following details:");
-        System.out.println("License number : " + licenseNo);
-        System.out.println("Pan card number : " + panCardNo);
-        System.out.println("Phone numbers : " + telephoneNo[0] + "\n" + telephoneNo[1]);
+        System.out.println("Congratulations " + getCustomerName() + "!!! you have been successfully registered for our services with the following details:");
+        System.out.println("License number : " + getLicenseNo());
+        System.out.println("Pan card number : " + getPanCardNo());
+        System.out.println("Phone numbers : " + getTelephoneNo()[0] + "\n" + getTelephoneNo()[1]);
+        System.out.println();
     }
     Registration(String customerName, int voterId, int licenseNo, long[] telephoneNo){
         this.customerName = customerName;
         this.voterId = voterId;
         this.licenseNo = licenseNo;
         this.telephoneNo = telephoneNo;
-        System.out.println("Congratulations " + customerName + " You have been successfully registered for our services with the following details:");
-        System.out.println("Voter id : " + voterId);
-        System.out.println("License number : " + licenseNo);
-        System.out.println("Phone numbers : " + telephoneNo[0] + "\n" + telephoneNo[1]);
+        System.out.println("Congratulations " + getCustomerName() + "!!! you have been successfully registered for our services with the following details:");
+        System.out.println("Voter id : " + getVoterId());
+        System.out.println("License number : " + getLicenseNo());
+        System.out.println("Phone numbers : " + getTelephoneNo()[0] + "\n" + getTelephoneNo()[1]);
+        System.out.println();
     }
     Registration(String customerName, String panCardNo, int voterId, long[] telephoneNo){
         this.customerName = customerName;
         this.panCardNo = panCardNo;
         this.voterId = voterId;
         this.telephoneNo = telephoneNo;
-        System.out.println("Congratulations " + customerName + " You have been successfully registered for our services with the following details:");
-        System.out.println("Pan card number : " + panCardNo);
-        System.out.println("Voter id : " + voterId);
-        System.out.println("Phone numbers : " + telephoneNo[0] + "\n" + telephoneNo[1]);
+        System.out.println("Congratulations " + getCustomerName() + "!!! you have been successfully registered for our services with the following details:");
+        System.out.println("Pan card number : " + getPanCardNo());
+        System.out.println("Voter id : " + getVoterId());
+        System.out.println("Phone numbers : " + getTelephoneNo()[0] + "\n" + getTelephoneNo()[1]);
+        System.out.println();
     }
 
-    
-    
+    public String getCustomerName(){
+        return customerName;
+    }
+    public String getPanCardNo(){
+        return panCardNo;
+    }
+    public int getVoterId(){
+        return voterId;
+    }
+    public String getPassportNo(){
+        return passportNo;
+    }   
+    public int getLicenseNo(){
+        return licenseNo;
+    }
+    public long[] getTelephoneNo(){
+        return telephoneNo;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Enigma internet service");
-        System.out.println("Please enter your details below");
+        Registration r1 = new Registration("Kevin", "MN9891N", new long[]{9452425421L, 7676765252L});
 
-        String name = Methods1.getCustomerName();
+        Registration r2 = new Registration("Julias", 123, "PN7878", new long[]{2345615451L, 6763562562L});
 
-        boolean status = Methods1.checkPass();
+        Registration r3 = new Registration("Jammy", 45453, 765, new long[]{9634524353L, 9887373737L});
 
-        if(status){
-            String passport = Methods1.getPassportNo();
-            long[] telephone = Methods1.getTelephoneNo();
-            Registration user = new Registration(name, passport, telephone);
-        }
-        else{
-            System.out.println("Please provide any one of the following combinations : ");
-            System.out.println("1. License number and pan card number.");
-            System.out.println("2. Voter id and license number.");
-            System.out.println("3. Pan card and voter id.");
-            System.out.println("Enter your choice : ");
-            int choice = input.nextInt();
-            int license;
-            int voterid;
-            long[] telephone;
-            String pancard;
-            switch(choice){
-                case 1:
-                    license = Methods1.getLicenseNo();
-                    pancard = Methods1.getPanCardNo();
-                    telephone = Methods1.getTelephoneNo();
-                    Registration user = new Registration(name, license, pancard, telephone);
-                    break;
-                case 2:
-                    voterid = Methods1.getVoterId();
-                    license = Methods1.getLicenseNo();
-                    telephone = Methods1.getTelephoneNo();
-                    Registration user2 = new Registration(name, voterid, license, telephone);
-                    break;
-                case 3:
-                    pancard = Methods1.getPanCardNo();
-                    voterid = Methods1.getVoterId();
-                    telephone = Methods1.getTelephoneNo();
-                    Registration user3 = new Registration(name, pancard, voterid, telephone);
-                    break;
-                default:
-                    System.out.println("Invalid choice");
-            }
-            
-        }
+        Registration r4 = new Registration("Rose", "PN8934", 34356, new long[]{9867456367L, 7645367356L});
     }
 
 }
