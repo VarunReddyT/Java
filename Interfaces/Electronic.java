0257ef78887e1c7d4f6dcb5886bef27d45b9e0f7
Varun-MSI <tvarun2014@gmail.com>
2024-04-26 11:58:37 +0530

    Interfaces

--- Interfaces/Electronic.java
+++ Interfaces/Electronic.java
@@ -24,12 +24,24 @@ public class Electronic {
     }
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone();
-        Computer c1 = new Computer("Varun", 32, 2);
+        Computer c1 = new Computer("HP", 32, 2);
         BluetoothInterface b = new Computer("Dull", 512, 1);
+
+        Hotspot h1 = new Computer("Lenovo",1024,16);
+        Hotspot h2 = new Computer("MSI",500,8);
+
+        
         s1.enableBluetooth();
         c1.powerOn();
         s1.powerOff();
         b.enableBluetooth();
+
+        System.out.println("\n");
+        
+        h1.enableHotspot();
+        h1.connectToHotspot("Varun");
+
+
     }
 }
 
@@ -54,7 +66,87 @@ interface BluetoothInterface {
     byte[] receiveData();
 }
 
-class Smartphone extends Electronic implements BluetoothInterface{
+
+interface Hotspot{
+    // Method to enable hotspot
+    void enableHotspot();
+
+    void getPassword();
+    // Method to disable hotspot
+    void disableHotspot();
+
+    // Method to connect to a device
+    boolean connectToHotspot(String deviceName);
+
+    // Method to disconnect from a connected device
+    void disconnectDeviceH();
+
+    // Method to send data to the connected device
+    void sendDataH(byte[] data);
+
+    // Method to receive data from the connected device
+    byte[] receiveDataH();
+}
+class Smartphone extends Electronic implements BluetoothInterface,Hotspot{
+    private boolean hotspotEnabled = false;
+
+    @Override
+    public void enableHotspot() {
+        hotspotEnabled = true;
+        System.out.println("Hotspot enabled.");
+    }
+
+    @Override
+    public void disableHotspot() {
+        hotspotEnabled = false;
+        System.out.println("Hotspot disabled.");
+    }
+
+    @Override
+    public boolean connectToHotspot(String deviceName) {
+        if (hotspotEnabled) {
+            System.out.println("Connecting to " + deviceName + "...");
+            // Simulate a successful connection
+            return true;
+        } else {
+            System.out.println("Please enable Hotspot first.");
+            return false;
+        }
+    }
+
+    @Override
+    public void disconnectDeviceH() {
+        if (hotspotEnabled) {
+            System.out.println("Disconnecting from device...");
+        } else {
+            System.out.println("Hotspot is not enabled. Cannot disconnect.");
+        }
+    }
+
+    @Override
+    public void sendDataH(byte[] data) {
+        if (hotspotEnabled) {
+            System.out.println("Sending data: " + new String(data));
+        } else {
+            System.out.println("Hotspot is not enabled. Cannot send data.");
+        }
+    }
+
+    @Override
+    public byte[] receiveDataH() {
+        if (hotspotEnabled) {
+            System.out.println("Receiving data...");
+            // Simulate receiving data
+            return "Hello, this is a test message.".getBytes();
+        } else {
+            System.out.println("Hotspot is not enabled. Cannot receive data.");
+            return new byte[0];
+        }
+    }
+    @Override
+    public void getPassword(){
+        System.out.println("Password: 1234");
+    }
     private boolean bluetoothEnabled = false;
 
     @Override
@@ -112,7 +204,66 @@ class Smartphone extends Electronic implements BluetoothInterface{
     }
 }
 
-class Computer extends Electronic implements BluetoothInterface {
+class Computer extends Electronic implements BluetoothInterface,Hotspot{
+    private boolean hotspotEnabled = false;
+
+    @Override
+    public void enableHotspot() {
+        hotspotEnabled = true;
+        System.out.println("Hotspot enabled.");
+    }
+
+    @Override
+    public void disableHotspot() {
+        hotspotEnabled = false;
+        System.out.println("Hotspot disabled.");
+    }
+
+    @Override
+    public boolean connectToHotspot(String deviceName) {
+        if (hotspotEnabled) {
+            System.out.println("Connecting to " + deviceName + "...");
+            // Simulate a successful connection
+            return true;
+        } else {
+            System.out.println("Please enable Hotspot first.");
+            return false;
+        }
+    }
+
+    @Override
+    public void disconnectDeviceH() {
+        if (hotspotEnabled) {
+            System.out.println("Disconnecting from device...");
+        } else {
+            System.out.println("Hotspot is not enabled. Cannot disconnect.");
+        }
+    }
+
+    @Override
+    public void sendDataH(byte[] data) {
+        if (hotspotEnabled) {
+            System.out.println("Sending data: " + new String(data));
+        } else {
+            System.out.println("Hotspot is not enabled. Cannot send data.");
+        }
+    }
+
+    @Override
+    public byte[] receiveDataH() {
+        if (hotspotEnabled) {
+            System.out.println("Receiving data...");
+            // Simulate receiving data
+            return "Hello, this is a test message.".getBytes();
+        } else {
+            System.out.println("Hotspot is not enabled. Cannot receive data.");
+            return new byte[0];
+        }
+    }
+    @Override
+    public void getPassword(){
+        System.out.println("Password: 1234");
+    }
     // State specific to computers
     private String brand;
     private int storage; // in gigabytes
@@ -198,3 +349,5 @@ class Computer extends Electronic implements BluetoothInterface {
         System.out.println("Bluetooth status: " + (bluetoothEnabled ? "Enabled" : "Disabled"));
     }
 }
+
+
