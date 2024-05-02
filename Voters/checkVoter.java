c357fda5b5002e8cb4ca46a2bc3a2c18a6cc7a80
Varun-MSI <tvarun2014@gmail.com>
2024-05-02 12:40:38 +0530

    New

---
+++ Voters/checkVoter.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+public class checkVoter {
+    public static void main(String[] args) throws IOException{
+        File f = new File("//run//media//varun-msi//Local Disk//Programming//Java//Voters//Hyderabad");
+        f.mkdir();
+        File f2 = new File(f,"Voters.txt");
+        f2.createNewFile();
+        Scanner scanner = new Scanner(System.in);
+        FileWriter fw = new FileWriter(f2);
+        for(int i = 0;i<5;i++){
+            String name = scanner.next();
+            int age = scanner.nextInt();
+            if(age<=60){
+                System.out.println("You are below 60. Cannot vote !");
+            }
+            else{
+                fw.append(name+" "+age+"\n");
+            }
+        }
+        fw.close();
+    }
+}
