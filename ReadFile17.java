ed881ec1d881b35d392252c57f25f02c94f51d57
Varun-MSI <tvarun2014@gmail.com>
2024-05-02 12:17:57 +0530

    New

---
+++ ReadFile17.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class ReadFile17 {
+    public static void main(String[] args) throws IOException{
+        FileReader f = new FileReader("sample.txt");
+        int data;
+        while((data = f.read()) != -1){
+            System.out.println((char)data);
+        }
+    }
+}
