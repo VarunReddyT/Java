0eec96b042f1bd0126b3bb5a76d9530978d0e7ca
Varun-MSI <tvarun2014@gmail.com>
2024-05-02 12:09:55 +0530

    New

---
+++ ReadFile16.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class ReadFile16{
+    public static void main(String[] args) throws IOException {
+        File f = new File("sample1.txt");
+        f.createNewFile();
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write("Hello Deepak".getBytes());
+        fos.close();
+    }
+
+}
