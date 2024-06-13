6909c935c12329001bb3ac992f73e8eca5373591
Varun-MSI <tvarun2014@gmail.com>
2024-06-13 12:39:56 +0530

    New

--- Practice/Boxing.java
+++ Practice/Boxing.java
@@ -1,7 +1,16 @@
+// Boxing is the process of converting a primitive data type to a wrapper class object.
+// Unboxing is the process of converting a wrapper class object to a primitive data type.
+// Auto-boxing is the process of converting a primitive data type to a wrapper class object automatically.
+
+
 public class Boxing {
     public static void main(String[] args) {
         int h = 90;
         Integer i  = 30;
         System.out.println(h + i);
+        
+        String s = "100";  // String is not a wwrapper class object. It is a class. 
+        
+        System.out.println(s);
     }
 }
---
+++ Practice/StringInterning.java
@@ -0,0 +1,13 @@
+// String Interning is a method of storing only one copy of each distinct String Value, which must be immutable.
+
+public class StringInterning{
+    public static void main(String[] args) {
+        String s = "hello",s1 = "hello";
+        String s2 = s+s1;
+        String s3 = s+s1;
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s3 == s2);
+        System.out.println(s3.equals(s2));
+    }
+}
