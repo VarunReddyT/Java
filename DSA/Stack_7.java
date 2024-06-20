84db44b668b91453b7b4aac58e6089ce2a500b33
Varun-MSI <tvarun2014@gmail.com>
2024-06-20 12:19:54 +0530

    New

---
+++ DSA/Stack_7.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Stack_7 {
+    public static void main(String[] args) {
+        List<Integer> list = new Stack<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+    }
+}
