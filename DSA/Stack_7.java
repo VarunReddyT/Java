6ff4782f16767e678e2d3a2697f3eb275004628a
Varun-MSI <tvarun2014@gmail.com>
2024-06-20 12:28:31 +0530

    New

--- DSA/Stack_7.java
+++ DSA/Stack_7.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 public class Stack_7 {
     public static void main(String[] args) {
-        List<Integer> list = new Stack<>();
+        Stack<Integer> list = new Stack<>();
         list.add(10);
         list.add(20);
         list.add(30);
         list.add(40);
+        list.push(30);        // If we use List interface, then this push method will not be available
         System.out.println(list);
     }
 }
