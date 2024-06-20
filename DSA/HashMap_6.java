d5f63ba42753a812969005eb1bb506abd26f0c7e
Varun-MSI <tvarun2014@gmail.com>
2024-06-20 12:02:39 +0530

    New

---
+++ DSA/HashMap_6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class HashMap_6 {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(100, "Amit");
+        map.put(101, "Vijay");
+        map.put(102, "Rahul");
+        for(Map.Entry<Integer, String> entry : map.entrySet()){
+            Integer key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " " + value);
+        }
+    }
+}   
