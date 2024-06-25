d95264ce23a72f8a29ffccf510ad2043e1a6e7eb
Varun-MSI <tvarun2014@gmail.com>
2024-06-25 21:29:04 +0530

    New

--- JDBC/jdbc_1.java
+++ JDBC/jdbc_1.java
@@ -12,9 +12,9 @@ public class jdbc_1 {
             while (rs.next()){
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
             }
-            while(rs2.next()){
-                System.out.println(rs2.getString(1));
-            }
+            // while(rs2.next()){
+            //     System.out.println(rs2.getString(1));
+            // }
             con.close();
         } catch (Exception e) {
             System.out.println(e);
