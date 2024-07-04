816586f8ccaccd72fd9140704ceae2128255fa53
Varun-MSI <tvarun2014@gmail.com>
2024-07-04 12:04:59 +0530

    New

---
+++ JDBC/jdbc_rollback_5.java
@@ -0,0 +1,52 @@
+// insert, delete, commit, rollback, savepoint
+
+import java.sql.*;
+public class jdbc_rollback_5 {
+    public static void main(String[] args) throws SQLException{
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/operations", "root","Tvkreddy@2014");
+            con.setAutoCommit(false);
+
+            // Inserting a record
+            String insertSQL = "INSERT INTO Students (FirstName,LastName,Gender,DateOfBirth,Email,PhoneNumber,Address,City,State,ZipCode,EnrollmentDate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(insertSQL);
+
+            pstmt.setString(1, "Varun");
+            pstmt.setString(2, "Reddy");
+            pstmt.setString(3, "M");
+            pstmt.setDate(4, Date.valueOf("2004-09-01"));
+            pstmt.setString(5, "tvarun@gmail.com");
+            pstmt.setString(6, "1234567890");
+            pstmt.setString(7, "Narayanguda");
+            pstmt.setString(8, "Hyderabad");
+            pstmt.setString(9, "Telangana");
+            pstmt.setString(10, "500092");
+            pstmt.setDate(11, Date.valueOf("2022-10-31"));
+
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            
+            // Savepoint
+            Savepoint savepoint = con.setSavepoint();
+
+            // Deleting a record
+            
+            String deleteSQL = "DELETE FROM Students WHERE StudentID = ?";
+            pstmt = con.prepareStatement(deleteSQL);
+            pstmt.setInt(1, 2);
+            pstmt.executeUpdate();
+            
+            // Rollback
+            con.rollback(savepoint);
+
+            con.commit();
+
+            con.close();    
+
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+    }
+}
