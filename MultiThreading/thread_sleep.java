a187835ca6961eaa2c9ea0bcc9c496689ae7914c
Varun-MSI <tvarun2014@gmail.com>
2024-07-09 12:10:24 +0530

    New

---
+++ MultiThreading/thread_sleep.java
@@ -0,0 +1,16 @@
+public class thread_sleep extends Thread{
+    public void run(){    
+        for(int i=1;i<=5;i++){   
+        // the thread will sleep for the 2000 milli seconds / 2 seconds   
+          try{Thread.sleep(2000);}catch(InterruptedException e){System.out.println(e);}    
+          System.out.println(i);    
+        }    
+       }    
+       public static void main(String args[]){    
+        thread_sleep t1= new thread_sleep();    
+        thread_sleep t2= new thread_sleep();    
+           
+        t1.start();    
+        t2.start();    
+       }    
+}
