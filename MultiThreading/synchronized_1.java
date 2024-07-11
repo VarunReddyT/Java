c87510f9d7f5386fb9438752e5802e640ba86dca
Varun-MSI <tvarun2014@gmail.com>
2024-07-11 12:14:51 +0530

    New

---
+++ MultiThreading/synchronized_1.java
@@ -0,0 +1,14 @@
+public class synchronized_1 extends Thread {
+    synchronized public void run(){
+        for(int i = 0;i<20;i++){
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+    public static void main(String[] args) {
+        synchronized_1 s = new synchronized_1();
+        Thread t1 = new Thread(s);
+        Thread t2 = new Thread(s);
+        t1.start();
+        t2.start();
+    }
+}
