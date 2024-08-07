cb36a955b4ad76032368ae088ef015f03d243da4
Varun-MSI <tvarun2014@gmail.com>
2024-08-07 20:46:09 +0530

    New

--- Lab_Programs_Manual/exp_6_b.java
+++ Lab_Programs_Manual/exp_6_b.java
@@ -5,7 +5,63 @@
 // PHP 2 
 // C# 1
 // Prolog 1
-
+import java.util.*;
+import java.io.*;
 public class exp_6_b {
-    
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        FileReader fr = new FileReader("logfile.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line = br.readLine();
+        
+
+        ArrayList<Data> arr = new ArrayList<>();
+        while((line = br.readLine())!=null){
+            String[] data = new String[3];
+            data[0] = line;
+            data[1] = br.readLine();
+            data[2] = br.readLine();
+            Data d = new Data(data[0],data[1],data[2]);
+            arr.add(d);
+        }
+        System.out.println("File Content : ");
+        for(Data i : arr){
+            System.out.println(i.progLang + " " + i.ip + " " + i.timeStamp);
+        }
+        int time1, time2;
+        int n = arr.size();
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                if(arr.get(i).progLang.equals(arr.get(j).progLang) && arr.get(i).ip.equals(arr.get(j).ip)){
+                    time1 = Integer.parseInt(arr.get(i).timeStamp);
+                    time2 = Integer.parseInt(arr.get(j).timeStamp);
+                    if(time2-time1 <= 20){
+                        arr.remove(j);
+                        n -= 1;
+                    }
+                }
+            }
+        }
+        HashMap<String,Integer> freq = new HashMap<>();
+        for(Data i : arr){
+            int c = freq.getOrDefault(i.progLang,0)+1;
+            freq.put(i.progLang,c);
+        }
+        System.out.println("Votes : ");
+        for(Map.Entry<String,Integer> m1 : freq.entrySet()){
+            System.out.println(m1.getKey() + " " + m1.getValue());
+        }
+        sc.close();
+        br.close();
+    }   
+}
+
+class Data{
+    String progLang, ip, timeStamp;
+    Data(String progLang, String ip, String timeStamp){
+        this.progLang = progLang;
+        this.ip = ip;
+        this.timeStamp = timeStamp;
+    }
 }
--- Lab_Programs_Manual/exp_8_b.java
+++ Lab_Programs_Manual/exp_8_b.java
@@ -1,6 +1,44 @@
 // b. Write a program to find the most common words in the list of words given in sorted order based on occurrence from largest to smallest. If any of words are having same occurrence then consider the smallest character order comes first.
 // Input format: First line contains the list of words and next line contains a number (k) which represent the top most words to display.
 // Output format: display the k top most words.
+import java.util.*;
 public class exp_8_b {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] words = s.split(" ");
+        int k = sc.nextInt();
+        int count = 0;
+        Map<String,Integer> freq = new HashMap<>();
+        for(int i = 0;i<words.length;i++){
+            int c = freq.getOrDefault(words[i],0) + 1;
+            freq.put(words[i],c);
+        }
+
+        Map<String,Integer> frequencies = new TreeMap<>(new Comparator<String>() {
+            public int compare(String k1, String k2){
+                int cmp = freq.get(k2).compareTo(freq.get(k1));
+                if(cmp == 0){
+                    return k1.compareTo(k2);
+                }
+                else{
+                    return cmp;
+                }
+            }
+        });
+
+        for(Map.Entry<String,Integer> m1 : freq.entrySet()){
+            frequencies.put(m1.getKey(),m1.getValue());
+        }
+
+        for(Map.Entry<String,Integer> m1 : frequencies.entrySet()){
+            System.out.print(m1.getKey()+" ");
+            count++;
+            if(count == k){
+                break;
+            }
+        }
+        sc.close();
+
+    }
 }
